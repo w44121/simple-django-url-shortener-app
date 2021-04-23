f685f1e106230953ded335407fdddd9902ffac5b
w44121 <46575438+w44121@users.noreply.github.com>
2021-04-23 17:21:08 +0300

    Update settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
  
 STATIC_URL = '/static/'
 
